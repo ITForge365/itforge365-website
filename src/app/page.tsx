52855f9b158f3ebf4b178c673f4906839c405ba5
InfoBimBim <karim.rezig@live.fr>
2026-09-15 16:10:41 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
         <h1 className="text-6xl font-bold mb-6">
-          ITForge365 INT
+          ITForge365  INT
         </h1>
         <p className="text-xl text-gray-300 text-center max-w-2xl">
            Scripts, automatisation et outils Microsoft 365
